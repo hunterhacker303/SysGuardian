67bdfea3134bb67b699670d38bb4a5781964cb0a
Harshit Namdev <harshitnamdev2006@gmail.com>
2026-03-15 20:44:05 +0530

    Process Analyzer Done

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import time
 import psutil
+import os
 
-def display_usage(cpu_usage, mem_usage, bars=50):
+def display_usage(cpu_usage, mem_usage,disk_usage, bars=50):
     cpu_percent = (cpu_usage / 100.0)
-    cpu_bar = '|' * int(cpu_percent * bars) + '-' * (bars - int(cpu_percent * bars))
+    cpu_bar = '#' * int(cpu_percent * bars) + '-' * (bars - int(cpu_percent * bars))
 
     mem_percent = (mem_usage / 100.0)
-    mem_bar = '|' * int(mem_percent * bars) + '-' * (bars - int(mem_percent * bars))
+    mem_bar = '#' * int(mem_percent * bars) + '-' * (bars - int(mem_percent * bars))
 
-    print(f"\rCPU Usage: |{cpu_bar}| {cpu_usage:.2f}% ", end="")
-    print(f"MEM Usage: |{mem_bar}| {mem_usage:.2f}% ", end="\r")
+    disk_percent=(disk_usage / 100.0)
+    disk_bar='#'*int(disk_percent*bars) +'-'*(bars-int(disk_percent *bars))
+
+    print(f"\rCPU Usage: |{cpu_bar}| {cpu_usage:.2f}% ")
+
+    print(f"MEM Usage: |{mem_bar}| {mem_usage:.2f}% ")
+
+    print(f"Disk Usage: |{disk_bar}| {disk_usage:.2f}% ")
+
 
 
 while True:
-    display_usage(psutil.cpu_percent(), psutil.virtual_memory().percent, 30)
+    os.system("clear")
+    display_usage(psutil.cpu_percent(), psutil.virtual_memory().percent,psutil.disk_usage('/').percent ,30)
+    
     time.sleep(0.5)
